343393162ac63a75589bb8245bd17aa7705f66ba
Alexander <worthalex.myvuw.ac.nz>
2024-04-26 10:27:00 +1200

    Please enter the commit message for your changes. Lines starting

--- ruby.cpp
+++ ruby.cpp
@@ -14,162 +14,51 @@ const int numCols = 320;
 const int numRowSamples = (int)(numCols / pxBetweenSamples);
 const int numColSamples = (int)(numRows / pxBetweenSamples);
 const int redThreshold = 40;
-const int rubyPxSize = 36;
-const int marginOfError = 2;
-const int acceptableErrors = 5;
 
-const char PositiveColour[3] = {255, 255, 0};   // yellow, shows up well on red
-const char NegativeColour[3] = {0, 128, 255}; // cyan, shows up well on black and white and gray and easily differentiable from yellow
-
-struct pixel
+bool check_if_different(double old, double nEw)
 {
-    int x;
-    int y;
-    int r;
-    //int g;
-    //int b;
-    //int v;
-    double relativeRed;
-    double relativeRedBuffer1 = 1298;
-    
-};
-
-int setPx(pixel targetPixel)
-{
-    // if (targetPixel.isRed)
-    // {
-    //     set_pixel(targetPixel.y, targetPixel.x, PositiveColour[0], PositiveColour[1], PositiveColour[2]);
-
-    //     //set_pixel(targetPixel.y+1, targetPixel.x, PositiveColour[0], PositiveColour[1], PositiveColour[2]);
-    //     //set_pixel(targetPixel.y+2, targetPixel.x, PositiveColour[0], PositiveColour[1], PositiveColour[2]);
-    //     //set_pixel(targetPixel.y-1, targetPixel.x, PositiveColour[0], PositiveColour[1], PositiveColour[2]);
-    //     //set_pixel(targetPixel.y-2, targetPixel.x, PositiveColour[0], PositiveColour[1], PositiveColour[2]);
-
-    //     //set_pixel(targetPixel.y, targetPixel.x+1, PositiveColour[0], PositiveColour[1], PositiveColour[2]);
-    //     //set_pixel(targetPixel.y, targetPixel.x+2, PositiveColour[0], PositiveColour[1], PositiveColour[2]);
-    //     //set_pixel(targetPixel.y, targetPixel.x-1, PositiveColour[0], PositiveColour[1], PositiveColour[2]);
-    //     //set_pixel(targetPixel.y, targetPixel.x-2, PositiveColour[0], PositiveColour[1], PositiveColour[2]);
-        
-    // }
-    // else
-    // {
-    //     set_pixel(targetPixel.y, targetPixel.x, NegativeColour[0], NegativeColour[1], NegativeColour[2]);
-
-    //     //set_pixel(targetPixel.y+1, targetPixel.x, NegativeColour[0], NegativeColour[1], NegativeColour[2]); // No crosshairs for negative pixels
-    //     //set_pixel(targetPixel.y+2, targetPixel.x, NegativeColour[0], NegativeColour[1], NegativeColour[2]); // to make it more easily differentiable
-    //     //set_pixel(targetPixel.y-1, targetPixel.x, NegativeColour[0], NegativeColour[1], NegativeColour[2]);
-    //     //set_pixel(targetPixel.y-2, targetPixel.x, NegativeColour[0], NegativeColour[1], NegativeColour[2]);
-
-    //     //set_pixel(targetPixel.y, targetPixel.x+1, NegativeColour[0], NegativeColour[1], NegativeColour[2]);
-    //     //set_pixel(targetPixel.y, targetPixel.x+2, NegativeColour[0], NegativeColour[1], NegativeColour[2]);
-    //     //set_pixel(targetPixel.y, targetPixel.x-1, NegativeColour[0], NegativeColour[1], NegativeColour[2]);
-    //     //set_pixel(targetPixel.y, targetPixel.x-2, NegativeColour[0], NegativeColour[1], NegativeColour[2]);
-    // }
+    if ((nEw + old)/ 2 > (old * 2) + redThreshold || (nEw + old)/ 2 > (old * 2) + redThreshold)
+    {
+        return true;
+    }
+    return false;
 }
 
 int main()
 {
     int err = init(0);
     std::cout << "Error: " << err << std::endl;
-
-    open_screen_stream();    
+    open_screen_stream();
     
-    int at = 0;
+    double old[4] = {
+        get_pixel(120, 140, 0) / get_pixel(120, 140, 3), // left
+        get_pixel(120, 180, 0) / get_pixel(120, 180, 3), // right
+        get_pixel(100, 160, 0) / get_pixel(100, 160, 3), // top
+        get_pixel(140, 160, 0) / get_pixel(140, 160, 3), // bottom
+    };
+    
     while (true)
     {
-        int errors = 0;
         take_picture();
-        convert_camera_to_screen();
-        for (int row = 0; row < numRows; row += pxBetweenSamples)
+        double nEw[4] = {
+            get_pixel(120, 140, 0) / get_pixel(120, 140, 3), // left
+            get_pixel(120, 180, 0) / get_pixel(120, 180, 3), // right
+            get_pixel(100, 160, 0) / get_pixel(100, 160, 3), // top
+            get_pixel(140, 160, 0) / get_pixel(140, 160, 3), // bottom
+        };
+
+        for (int i = 0; i <= 3; i++)
         {
-            for (int col = 0; col < numCols; col += pxBetweenSamples)
+            if (check_if_different(old[i], nEw[i]))
             {
-                pixel sample = {col,
-                row,
-                get_pixel(sample.y, sample.x, 0),
-                (double)sample.r /((get_pixel(sample.y, sample.x, 1) + get_pixel(sample.y, sample.x, 2) + redThreshold )/ 2)};
-
-                if (sample.relativeRedBuffer1 != 1298)
-                {
-                    errors += (sample.relativeRed < sample.relativeRedBuffer1 - marginOfError ||
-                    sample.relativeRed > sample.relativeRedBuffer1 + marginOfError ) ? 1 : 0;
-
-
-                }
-                std::cout << "buffrr = " + std::to_string(sample.relativeRedBuffer1);
-                std::cout << " | relred = " + std::to_string(sample.relativeRed) << std::endl;
-                sample.relativeRedBuffer1 = sample.relativeRed;
-                set_pixel(sample.y, sample.x, sample.relativeRed,sample.relativeRed,sample.relativeRed);
+                std::cout << ("ruby stolen") << std::endl;
+                stoph();
             }
         }
-        std::cout << errors << std::endl;
-
-        if (errors > acceptableErrors)
-        {
-            std::cout << "RUBY STOLEN" << std::endl;
-        }
-
-
-
-
-        // int numRedPx = 0;
-        // for (int row = 0; row < numRows; row += pxBetweenSamples)
-        // {
-        //     for (int col = 0; col < numCols; col += pxBetweenSamples)
-        //     {
-        //         pixel sample = {col, row, get_pixel(sample.y, sample.x, 0),
-        //         get_pixel(sample.y, sample.x, 1),get_pixel(sample.y, sample.x, 2),get_pixel(sample.y, sample.x, 3),
-        //         (sample.r > (sample.g + sample.b + redThreshold )/ 2) ? true : false};
-        //         if (sample.isRed) {numRedPx++;}
-        //         setPx(sample);
-        //     }
-        // }
-
-        // if (numRedPx < rubyPxSize - marginOfError)
-        // {
-        //     std::cout << "NO RUBY DETECTED : ALERT! THIEF!" << std::endl;
-        // }
-        // else if (numRedPx > rubyPxSize + marginOfError)
-        // {
-        //     std::cout << "DUBLICATE RUBY DETECTED : ALERT! THEIF!" << std::endl;
-        // }
-        update_screen();
-        sleep1(1);
-        at++;
-        if (at > 5)
-        {
-            std::cout << "stop" << std::endl;
-            stoph();
-            break;
-        }
+        
     }
 
-    // while (true)
-    // {
-    //     take_picture();
-
-    //     for (int i; i < numColSamples; i++)
-    //     {
-    //         for (int j; j < numRowSamples; j++)
-    //         {
-    //             int pixelRed = get_pixel(i * pxBetweenSamples, j * pxBetweenSamples, 0) - '0'; // subtract '0' to turn chars into ints for some reason
-    //             setPx(pixelRed, redThreshold, i * pxBetweenSamples, j * pxBetweenSamples);
-    //         }
-    //         int pixelRed = get_pixel(i * pxBetweenSamples, columns, 0) - '0'; // check px on very edge for redundancy
-    //         setPx(pixelRed, redThreshold, i * pxBetweenSamples, columns);
-    //     }
-    //     for (int j; j < numRowSamples; j++)
-    //     {
-    //         int pixelRed = get_pixel(rows, j * pxBetweenSamples, 0) - '0'; // check px on very corner for redundancy
-    //         setPx(pixelRed, redThreshold, rows, j * pxBetweenSamples);
-    //     }
-    //     int pixelRed = get_pixel(rows, columns, 0) - '0'; // check px on very corner for redundancy
-    //     setPx(pixelRed, redThreshold, rows, columns);
-
-    //     convert_camera_to_screen();
-    //     update_screen();
-    // }
-
+    update_screen();
     close_screen_stream();
     return 0;
 }
