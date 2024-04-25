ea49260d7e377790a5fd9480dccb9945457930f2
Alexander <worthalex.myvuw.ac.nz>
2024-04-26 11:15:18 +1200

    Please enter the commit message for your changes. Lines starting

--- ruby.cpp
+++ ruby.cpp
@@ -23,8 +23,8 @@ const int bot = centerRow + offset;
 const int lft = centerCol - offset;
 const int rgt = centerCol + offset;
 
-const double relRed = 2.7;
-const int margin = 0.5;
+const double relRed = 2.07;
+const double margin = 0.4;
 
 int main()
 {
@@ -33,51 +33,51 @@ int main()
     open_screen_stream();
     take_picture();
 
-    while (true)
-    {
-        take_picture();
-        std::cout << ((double)get_pixel(centerRow, centerCol, 0) / (double)get_pixel(centerRow, centerCol, 3)) << std::endl;
-        sleep1(1000);
-        convert_camera_to_screen();
-        update_screen();
-    }
-    // double initRelRed = (double)get_pixel(centerRow, centerCol, 0) / (double)get_pixel(centerRow, centerCol, 3);
-    // if (initRelRed > relRed + margin || initRelRed < relRed - margin)
-    // {
-    //     std::cout << "ALERT!!! Ruby missing at startup!" << std::endl;
-    //     stoph();
-    // }
-    // // std::cout << ("arr1") << std::endl;
-    
     // while (true)
     // {
-    //     // std::cout << ("while") << std::endl;
     //     take_picture();
+    //     std::cout << ((double)get_pixel(centerRow, centerCol, 0) / (double)get_pixel(centerRow, centerCol, 3)) << std::endl;
+    //     sleep1(1000);
     //     convert_camera_to_screen();
-
-    //     double pixels[4] = {
-    //         (double)get_pixel(centerRow, lft, 0) / (double)get_pixel(centerRow, lft, 3) + 1, // left
-    //         (double)get_pixel(centerRow, rgt, 0) / (double)get_pixel(centerRow, rgt, 3) + 1, // right
-    //         (double)get_pixel(top, centerCol, 0) / (double)get_pixel(top, centerCol, 3) + 1, // top
-    //         (double)get_pixel(bot, centerCol, 0) / (double)get_pixel(bot, centerCol, 3) + 1, // bottom
-    //     };
-    //     // std::cout << ("arr2") << std::endl;
-    //     for (int i = 0; i <= 3; i++)
-    //     {
-    //         if (pixels[i] > relRed + margin || pixels[i] < relRed - margin)
-    //         {
-    //             std::cout << ("ALERT!!! Ruby stolen!") << std::endl;
-    //             stoph();
-    //         }
-    //     }
-        
-    //     set_pixel(centerRow, lft, 100, 255, 100);
-    //     set_pixel(centerRow, rgt, 100, 255, 100);
-    //     set_pixel(top, centerCol, 100, 255, 100);
-    //     set_pixel(bot, centerCol, 100, 255, 100);
     //     update_screen();
+    // }
+    double initRelRed = (double)get_pixel(centerRow, centerCol, 0) / (double)get_pixel(centerRow, centerCol, 3);
+    if (initRelRed > relRed + margin || initRelRed < relRed - margin)
+    {
+        std::cout << "ALERT!!! Ruby missing at startup!" << std::endl;
+        stoph();
+    }
+    // std::cout << ("arr1") << std::endl;
+    
+    while (true)
+    {
+        // std::cout << ("while") << std::endl;
+        take_picture();
+        convert_camera_to_screen();
+
+        double pixels[4] = {
+            (double)get_pixel(centerRow, lft, 0) / (double)get_pixel(centerRow, lft, 3) + 1, // left
+            (double)get_pixel(centerRow, rgt, 0) / (double)get_pixel(centerRow, rgt, 3) + 1, // right
+            (double)get_pixel(top, centerCol, 0) / (double)get_pixel(top, centerCol, 3) + 1, // top
+            (double)get_pixel(bot, centerCol, 0) / (double)get_pixel(bot, centerCol, 3) + 1, // bottom
+        };
+        // std::cout << ("arr2") << std::endl;
+        for (int i = 0; i <= 3; i++)
+        {
+            if (pixels[i] > relRed + margin || pixels[i] < relRed - margin)
+            {
+                std::cout << ("ALERT!!! Ruby stolen!") << std::endl;
+                stoph();
+            }
+        }
         
-    //}
+        set_pixel(centerRow, lft, 100, 255, 100);
+        set_pixel(centerRow, rgt, 100, 255, 100);
+        set_pixel(top, centerCol, 100, 255, 100);
+        set_pixel(bot, centerCol, 100, 255, 100);
+        update_screen();
+        
+    }
     
     close_screen_stream();
     return 0;
