eda02007ffdadcd220713d3c7c00e9f4be2f7f18
Alexander <worthalex.myvuw.ac.nz>
2024-04-26 11:05:11 +1200

    Please enter the commit message for your changes. Lines starting

--- ruby.cpp
+++ ruby.cpp
@@ -13,7 +13,6 @@
 // const int numCols = 320;
 // const int numRowSamples = (int)(numCols / pxBetweenSamples);
 // const int numColSamples = (int)(numRows / pxBetweenSamples);
-const int redThreshold = 2;
 const int centerRow = 120;
 const int centerCol = 160;
 
@@ -24,17 +23,8 @@ const int bot = centerRow + offset;
 const int lft = centerCol - offset;
 const int rgt = centerCol + offset;
 
-
-bool check_if_different(double old, double nEw)
-{
-    // std::cout << ("call") << std::endl;
-    if ((nEw + old)/ 2 > (old * 2) + redThreshold || (nEw + old)/ 2 > (old * 2) + redThreshold)
-    {
-        // std::cout << ("gorg") << std::endl;
-        return true;
-    }
-    return false;
-}
+const double relRed = 2.7;
+const int margin = 0.5;
 
 int main()
 {
@@ -42,27 +32,21 @@ int main()
     std::cout << "Error: " << err << std::endl;
     open_screen_stream();
     take_picture();
-    std::cout << (double)get_pixel(centerRow, lft, 0) / (double)get_pixel(centerRow, lft, 3) + 1 << std::endl;
-    double old[4] = {
-        (double)get_pixel(centerRow, lft, 0) / (double)get_pixel(centerRow, lft, 3) + 1, // left
-        (double)get_pixel(centerRow, rgt, 0) / (double)get_pixel(centerRow, rgt, 3) + 1, // right
-        (double)get_pixel(top, centerCol, 0) / (double)get_pixel(top, centerCol, 3) + 1, // top
-        (double)get_pixel(bot, centerCol, 0) / (double)get_pixel(bot, centerCol, 3) + 1, // bottom
-    };
+    double initRelRed = (double)get_pixel(centerRow, centerCol, 0) / (double)get_pixel(centerRow, centerCol, 3);
+    if (initRelRed > relRed + margin || initRelRed < relRed - margin)
+    {
+        std::cout << "ALERT!!! Ruby missing at startup!" << std::endl;
+        stoph();
+    }
     // std::cout << ("arr1") << std::endl;
     
     while (true)
     {
         // std::cout << ("while") << std::endl;
         take_picture();
-        set_pixel(centerRow, lft, 100, 255, 100);
-        set_pixel(centerRow, rgt, 100, 255, 100);
-        set_pixel(top, centerCol, 100, 255, 100);
-        set_pixel(bot, centerCol, 100, 255, 100);
         convert_camera_to_screen();
-        update_screen();
 
-        double nEw[4] = {
+        double pixels[4] = {
             (double)get_pixel(centerRow, lft, 0) / (double)get_pixel(centerRow, lft, 3) + 1, // left
             (double)get_pixel(centerRow, rgt, 0) / (double)get_pixel(centerRow, rgt, 3) + 1, // right
             (double)get_pixel(top, centerCol, 0) / (double)get_pixel(top, centerCol, 3) + 1, // top
@@ -71,12 +55,18 @@ int main()
         // std::cout << ("arr2") << std::endl;
         for (int i = 0; i <= 3; i++)
         {
-            if (check_if_different(old[i], nEw[i]))
+            if (check_if_different(pixels[i] > relRed + margin || pixels[i] < relRed - margin))
             {
-                std::cout << ("ruby stolen") << std::endl;
+                std::cout << ("ALERT!!! Ruby stolen!") << std::endl;
                 stoph();
             }
         }
+        
+        set_pixel(centerRow, lft, 100, 255, 100);
+        set_pixel(centerRow, rgt, 100, 255, 100);
+        set_pixel(top, centerCol, 100, 255, 100);
+        set_pixel(bot, centerCol, 100, 255, 100);
+        update_screen();
         
     }
     
