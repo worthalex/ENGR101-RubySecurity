85894ec9cad9c43221c762bff972037113b3a248
worthalex <worthalex@myvuw.ac.nz>
2024-04-26 16:29:45 +1200

    Please enter the commit message for your changes. Lines starting

--- ruby.cpp
+++ ruby.cpp
@@ -18,13 +18,17 @@ const int centerCol = 160;
 
 const int offset = 20;
 
-const int top = centerRow - offset;
-const int bot = centerRow + offset;
-const int lft = centerCol - offset;
-const int rgt = centerCol + offset;
+const int rubyTop = centerRow - offset;
+const int rubyBottom = centerRow + offset;
+const int rubyLeft = centerCol - offset;
+const int rubyRight = centerCol + offset;
 
-const double relRed = 4.9;
-const double margin = 3.2;
+const int surroundingTop = centerRow - 2*offset;
+const int surroundingBottom = centerRow + 2*offset;
+const int surroundingLeft = centerCol - 2*offset;
+const int surroundingRight = centerCol + 2*offset;
+
+const double red = 1.7;
 
 int main()
 {
@@ -62,39 +66,77 @@ int main()
         take_picture();
         convert_camera_to_screen();
 
-        double pixels[4] = {
-            (double)get_pixel(centerRow, lft, 0) / (double)get_pixel(centerRow, lft, 3), // left
-            (double)get_pixel(centerRow, rgt, 0) / (double)get_pixel(centerRow, rgt, 3), // right
-            (double)get_pixel(top, centerCol, 0) / (double)get_pixel(top, centerCol, 3), // top
-            (double)get_pixel(bot, centerCol, 0) / (double)get_pixel(bot, centerCol, 3), // bottom
-        };
+        double rubyPixels[4] = {
+            (double)get_pixel(centerRow, rubyLeft, 0) / (double)get_pixel(centerRow, rubyLeft, 3), // left
+            (double)get_pixel(centerRow, rubyRight, 0) / (double)get_pixel(centerRow, rubyRight, 3), // right
+            (double)get_pixel(rubyTop, centerCol, 0) / (double)get_pixel(rubyTop, centerCol, 3), // top
+            (double)get_pixel(rubyBottom, centerCol, 0) / (double)get_pixel(rubyBottom, centerCol, 3), // bottom
+        }; int rubyPixelsSize = sizeof(rubyPixels) / sizeof(double);
+
+        double surrPixels[8] = {
+            (double)get_pixel(centerRow, surroundingLeft, 0) / (double)get_pixel(centerRow, surroundingLeft, 3), // left
+            (double)get_pixel(centerRow, surroundingRight, 0) / (double)get_pixel(centerRow, surroundingRight, 3), // right
+            (double)get_pixel(surroundingTop, centerCol, 0) / (double)get_pixel(surroundingTop, centerCol, 3), // top
+            (double)get_pixel(surroundingBottom, centerCol, 0) / (double)get_pixel(surroundingBottom, centerCol, 3), // bottom
+            (double)get_pixel(surroundingTop, surroundingRight, 0) / (double)get_pixel(surroundingTop, surroundingRight, 3), // top right
+            (double)get_pixel(surroundingBottom, surroundingLeft, 0) / (double)get_pixel(surroundingBottom, surroundingLeft, 3), // bottom left
+            (double)get_pixel(surroundingTop, surroundingLeft, 0) / (double)get_pixel(surroundingTop, surroundingLeft, 3), // top left
+            (double)get_pixel(surroundingBottom, surroundingRight, 0) / (double)get_pixel(surroundingBottom, surroundingRight, 3), // bottom right
+        }; int surrPixelsSize = sizeof(rubyPixels) / sizeof(double);
         // std::cout << ("arr2") << std::endl;
 
-        set_pixel(centerRow, lft, 100, 255, 100);
-        set_pixel(centerRow, rgt, 100, 255, 100);
-        set_pixel(top, centerCol, 100, 255, 100);
-        set_pixel(bot, centerCol, 100, 255, 100);
+        set_pixel(centerRow, rubyLeft, 100, 255, 100);
+        set_pixel(centerRow, rubyRight, 100, 255, 100);
+        set_pixel(rubyTop, centerCol, 100, 255, 100);
+        set_pixel(rubyBottom, centerCol, 100, 255, 100);
+
+        set_pixel(centerRow, surroundingLeft, 100, 100, 255);
+        set_pixel(centerRow, surroundingRight, 100, 100, 255);
+        set_pixel(surroundingTop, centerCol, 100, 100, 255);
+        set_pixel(surroundingBottom, centerCol, 100, 100, 255);
+        set_pixel(surroundingTop, surroundingLeft, 100, 100, 255);
+        set_pixel(surroundingTop, surroundingRight, 100, 100, 255);
+        set_pixel(surroundingBottom, surroundingLeft, 100, 100, 255);
+        set_pixel(surroundingBottom, surroundingRight, 100, 100, 255);
         update_screen();
 
-        for (int i = 0; i <= 3; i++)
+        for (int i = 0; i < rubyPixelsSize; i++)
         {
             // std::cout << " " << std::endl;
             // std::cout << i << std::endl;
             // std::cout << pixels[i] << std::endl;
             // std::cout << relRed; std::cout << " +- "; std::cout << margin << std::endl;
-            if (pixels[i] > relRed + margin || pixels[i] < relRed - margin)
+            if (rubyPixels[i] < red)
             {
-                if (firstFrame)
+                if (!firstFrame)
                 {
-                    std::cout << ("ALERT!!! Ruby not present at startup!") << std::endl;
+                    std::cout << ("ALERT!!! Ruby no longer detected; Ruby stolen!") << std::endl;
+
                 } else 
                 {
-                    std::cout << ("ALERT!!! Ruby stolen!") << std::endl;
+                    std::cout << ("Error! Ruby not present at startup!") << std::endl;
                 }
                 stoph();
                 return 0;
             }
         }
+        for (int i = 0; i < surrPixelsSize; i++)
+        {
+            if (surrPixels[i] > red)
+            {
+                if (!firstFrame)
+                {
+                    std::cout << ("ALERT!!! Multiple rubies detected; Ruby stolen!") << std::endl;
+                } else
+                {
+                    std::cout << ("Error! Multiple rubies present at startup!") << std::endl;
+                }
+                stoph();
+                return 0;
+
+            }
+        } 
+
         if (firstFrame)
         {
             std::cout << ("Ruby monitor started, ruby is present") << std::endl;
