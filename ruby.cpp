242a931896b61902636002757e7736dc22a68d08
worthalex <worthalex@myvuw.ac.nz>
2024-04-26 18:06:27 +1200

    Please enter the commit message for your changes. Lines starting

--- ruby.cpp
+++ ruby.cpp
@@ -78,9 +78,9 @@ int main()
                 std::cout << ruby1.col << std::endl;
                 std::cout << row << std::endl;
                 std::cout << col << std::endl;
-                ruby2 = {row+pxBetweenSamples, col};
-                ruby3 = {row, col+pxBetweenSamples};
-                ruby4 = {row+pxBetweenSamples, col+pxBetweenSamples}; 
+                ruby2 = {row+surrOffset, col};
+                ruby3 = {row, col+surrOffset};
+                ruby4 = {row+surrOffset, col+surrOffset}; 
                 set_pixel(row, col, 100, 255, 100);
                 foundRuby = true;
                 goto exit;
